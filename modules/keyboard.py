f0612451dd87ab387e7b970d3eb1123ea80cefdc
andreybashtovoy <rovage.abc@gmail.com>
2021-04-18 00:08:15 +0300

    Basic project structure

---
+++ modules/keyboard.py
@@ -0,0 +1,26 @@
+import logging
+
+from aiogram import Bot, Dispatcher, executor, types
+from constants import buttons
+
+
+def get_keyboard():
+    markup = types.ReplyKeyboardMarkup(
+        resize_keyboard=True,
+        row_width=1,
+        one_time_keyboard=True
+    )
+
+    markup.row(
+        types.KeyboardButton(buttons.START_ACTIVITY),
+        types.KeyboardButton(buttons.START_PROJECT)
+    )
+
+    markup.add(
+        types.KeyboardButton(buttons.ADD_TIME),
+        types.KeyboardButton(buttons.MY_ACTIVITIES),
+        types.KeyboardButton(buttons.MY_STATS)
+    )
+
+    return markup
+
