45234805924b83f58f4fd33f40a09b4a25bd7a32
andreybashtovoy <rovage.abc@gmail.com>
2021-04-24 22:33:38 +0300

    check if user and chat exists

--- database/user.py
+++ database/user.py
@@ -1,11 +1,31 @@
-from .models import User
+from .models import User, Chat, ChatXUser
 
 
-async def check_user(user_id, username):
-    exists = await User.query.where(User.username == username).gino.first()
+async def check_user_and_chat(user_id, username, chat_id, chat_name):
+    # Проверка есть ли пользователь в базе
+    user_exists = await User.query.where(User.user_id == user_id).gino.first()
 
-    if not exists:
+    if not user_exists:
         await User.create(
             user_id=user_id,
             username=username
+        )
+
+    # Проверка есть ли чат в базе
+    chat_exists = await Chat.query.where(Chat.chat_id == chat_id).gino.first()
+
+    if not chat_exists:
+        await Chat.create(
+            chat_id=chat_id,
+            name=chat_name
+        )
+
+    # Проверка есть ли соотношение чата и пользователя в базе
+    query = ChatXUser.query.where(ChatXUser.chat_id == chat_id and ChatXUser.user_id == user_id)
+    chat_x_user_exists = await query.gino.first()
+
+    if not chat_x_user_exists:
+        await ChatXUser.create(
+            chat_id=chat_id,
+            user_id=user_id
         )
--- handlers/start.py
+++ handlers/start.py
@@ -4,15 +4,25 @@ from aiogram.dispatcher import FSMContext
 
 from loader import dp
 from modules import keyboard
-from database.user import check_user
+from database.user import check_user_and_chat
 from states import States
 
 
 @dp.message_handler(CommandStart(), state='*')
 async def bot_start(message: types.Message, state: FSMContext):
-    await message.answer(f"Привет, {message.from_user.full_name}!",
-                         reply_markup=keyboard.get_keyboard())
+    # Проверка, есть ли пользователь и чат в базе
+    await check_user_and_chat(
+        user_id=message.from_user.id,
+        username=message.from_user.username,
+        chat_id=message.chat.id,
+        chat_name=message.chat.full_name
+    )
 
-    await check_user(message.from_user.id, message.from_user.username)
-
+    # Установка состояния главного меню
     await States.MAIN_MENU.set()
+
+    # Получение клавиатуры главного меню
+    markup = await keyboard.get_keyboard(state)
+
+    await message.answer(f"Привет, {message.from_user.full_name}!",
+                         reply_markup=markup)
