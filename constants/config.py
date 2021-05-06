1a1479569af7769c39242bb4d1ba68a7e90e038f
andreybashtovoy <rovage.abc@gmail.com>
2021-05-06 19:05:13 +0300

    DB config

--- constants/config.py
+++ constants/config.py
@@ -7,4 +7,8 @@ BOT_TOKEN = env.str("BOT_TOKEN")
 ADMINS = env.list("ADMINS")
 API_PORT = env.str("API_PORT")
 
-POSTGRES_URI = "postgresql://postgres:kpi@localhost:5432/time_player"
+DB_NAME = env.str("DB_NAME")
+DB_LOGIN = env.str("DB_LOGIN")
+DB_PASSWORD = env.str("DB_PASSWORD")
+
+POSTGRES_URI = f"postgresql://{DB_LOGIN}:{DB_PASSWORD}@localhost:5432/{DB_NAME}"
