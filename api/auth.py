3ebc6aa0d6461b347502f0c75919da21d7ca5717
andreybashtovoy <rovage.abc@gmail.com>
2021-05-06 19:50:56 +0300

    test

--- api/auth.py
+++ api/auth.py
@@ -7,6 +7,19 @@ from constants import config
 from loader import routes
 
 
+@routes.get('/')
+async def tewr(request):
+    return web.Response(text="""
+    <script async src="https://telegram.org/js/telegram-widget.js?14" data-telegram-login="timeplayer_sdbot" data-size="large" data-onauth="onTelegramAuth(user)" data-request-access="write"></script>
+    <script type="text/javascript">
+      function onTelegramAuth(user) {
+        alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
+      }
+    </script>
+    """,
+                        content_type='text/html')
+
+
 @routes.get('/auth')
 async def hello(request):
     data = await request.post()
