cde284a0bb23823737570026337d9db69554a969
andreybashtovoy <rovage.abc@gmail.com>
2021-05-27 23:25:48 +0300

    zxc

--- main.py
+++ main.py
@@ -9,14 +9,27 @@ import api
 
 from database.loader import load_db
 from constants import config
+from aiohttp_middlewares import (
+    cors_middleware
+)
+from aiohttp_middlewares.cors import DEFAULT_ALLOW_HEADERS
 
-app = web.Application()
+app = web.Application(middlewares=(
+    [cors_middleware(
+        #origins=["https://manage.sslforfree.com/", "http://timeplayer-new.loca.lt/", "http://timeplayer.loca.lt/"],
+        allow_all=True,
+        allow_credentials=True,
+        allow_methods=("POST", "PATCH", "GET"),
+        allow_headers=DEFAULT_ALLOW_HEADERS
+                      + ("X-Client-UID",),
+    )]
+))
 
 app.add_routes(routes)
 
 
 async def on_startup(*args):
-    await load_db()  # Подключение базы данных.loca.lt/
+    await load_db()  # Подключение базы данных
 
     # Запуск API-сервера
     runner = web.AppRunner(app)
