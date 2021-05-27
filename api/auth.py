002970652195023eb2c626fccb56f153cce920cf
andreybashtovoy <rovage.abc@gmail.com>
2021-05-27 14:29:30 +0300

    secure cookie

--- api/auth.py
+++ api/auth.py
@@ -54,7 +54,8 @@ async def hello(request):
             max_age=60*60*24*30,
             httponly=True,
             #domain=".timeplayer-new.loca.lt"
-            samesite="None"
+            samesite="None",
+            secure=True
         )
 
         return response
